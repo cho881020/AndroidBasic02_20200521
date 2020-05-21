8183c0ded436c5173e40e0ec373470d44d581e64
kj_cho <kj_cho@idealidea.co.kr>
2020-05-21 12:36:37 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/androidbasic02_20200521/BaseActivity.java
@@ -0,0 +1,9 @@
+package kr.co.tjoeun.androidbasic02_20200521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+}
